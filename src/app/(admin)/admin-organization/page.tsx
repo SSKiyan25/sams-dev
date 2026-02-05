46cb1f5067f232d92db6309c38e41f3c33bb63d9
John Rhuel Laurente <22-1-00109@vsu.edu.ph>
2026-02-05 17:28:12 +0800

    Implements the new admin account with levelAccess of 3
    This new type of user is dedicated for USSC for managing organizations and students accounts.

---
+++ src/app/(admin)/admin-organization/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function AdminOrganizationPage() {
+    return (
+        <div>
+            <h1>Admin Organization...</h1>
+            <p>To be filled with admin organization page content.</p>
+        </div>
+    );
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,43 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const protectedRoutes = ["/org-dashboard", "/org-events", "/org-members"];
 const publicRoutes = ["/login", "/"];
+const level1and2Routes = ["/org-dashboard", "/org-events", "/org-members"];
+const level3Routes = ["/admin-dashboard", "/admin-students", "/admin-organization"];
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const token = request.cookies.get("session")?.value || null;
+  const accessLevel = request.cookies.get("accessLevel")?.value || null;
   const isAuthenticated = !!token;
 
-  if (isAuthenticated && publicRoutes.includes(pathname)) {
-    return NextResponse.redirect(new URL("/org-dashboard", request.url));
+  if(isAuthenticated && publicRoutes.includes(pathname)) {
+    if (accessLevel === "3") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    } else {
+      return NextResponse.redirect(new URL("/org-dashboard", request.url));
+    } 
   }
 
-  if (
-    !isAuthenticated &&
-    protectedRoutes.some((route) => pathname.startsWith(route))
-  ) {
+  const isProtectedRoute = level1and2Routes.some((route) => pathname.startsWith(route)) ||
+    level3Routes.some((route) => pathname.startsWith(route));
+  
+  if (!isAuthenticated && isProtectedRoute) {
     return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (isAuthenticated) {
+    if (level3Routes.some(route => pathname.startsWith(route))) {
+      if (accessLevel !== "3") {
+        return NextResponse.redirect(new URL("/org-dashboard", request.url));
+      }
+    }
+    else if (level1and2Routes.some(route => pathname.startsWith(route))) {
+      if (accessLevel !== "2" && accessLevel !== "1") {
+        return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+      }
+    }
   }
 
   return NextResponse.next();
